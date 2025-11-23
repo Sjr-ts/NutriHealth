8e233a81c9d7fc605742260e1c03eb6dc1b817a1
Sjr-ts <154641686+Sjr-ts@users.noreply.github.com>
2025-11-23 06:09:48 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,6 @@
-// build.gradle.kts (Project: NutriH)
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
-
-    // --- NOVO PLUGIN ---
-    // Plugin do Google Services para o Firebase
     alias(libs.plugins.google.gms.google.services) apply false
 }
